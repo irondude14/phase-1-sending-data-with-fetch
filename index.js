77288541707738a9c0fee0d94e7d6c9b9e18b34e
Vladyslav <vkadyrov@protonmail.com>
2022-10-11 21:10:46 -0400

    commit tree 511aabda4e1eec30d4b94229623ffc690aad8e23

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+function submitData(name, email) {
+    const postData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    }
+    return fetch('http://localhost:3000/users', postData)
+    .then(res => res.json())
+    .then(newUser => document.body.innerHTML = newUser.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
